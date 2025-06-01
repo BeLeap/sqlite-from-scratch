32de20a8eba64b0dd8cf433466b52b6c4edc6090
BeLeap <beleap@beleap.dev>
2025-06-01 15:26:35 +0900

    update statement execution

--- main.c
+++ main.c
@@ -145,15 +145,36 @@ void* row_slot(Table* table, uint32_t row_num) {
   uint32_t byte_offset = row_offset * ROW_SIZE;
   return page + byte_offset;
 }
+typedef enum {
+  EXECUTE_SUCCESS,
+  EXECUTE_TABLE_FULL,
+} ExecuteResult;
 
-void execute_statement(Statement* statement) {
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+  if (table->num_rows >= TABLE_MAX_ROWS) {
+    return EXECUTE_TABLE_FULL;
+  }
+
+  Row* row_to_insert = &(statement->row_to_insert);
+  serialize_row(row_to_insert, row_slot(table, table->num_rows));
+  table->num_rows+=1;
+
+  return EXECUTE_SUCCESS;
+}
+ExecuteResult execute_select(Statement* statement, Table* table) {
+  Row row;
+  for (uint32_t i = 0; i < table->num_rows; i++) {
+    deserialize_row(row_slot(table, i), &row);
+    print_row(&row);
+  }
+  return EXECUTE_SUCCESS;
+}
+ExecuteResult execute_statement(Statement* statement, Table* table) {
   switch (statement->type) {
-    case (STATEMENT_INSERT):
-      printf("This is where we would do an insert.\n");
-      break;
+    case STATEMENT_INSERT:
+      return execute_insert(statement, table);
     case STATEMENT_SELECT:
-      printf("This is where we would do a select.\n");
-      break;
+      return execute_select(statement, table);
   }
 }
 
