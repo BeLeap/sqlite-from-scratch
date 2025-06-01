745c90774c0046d4c39a40cceb5e8c2515966fea
BeLeap <beleap@beleap.dev>
2025-06-01 15:29:20 +0900

    create & destroy table

--- main.c
+++ main.c
@@ -134,6 +134,22 @@ typedef struct {
   void* pages[TABLE_MAX_PAGES];
 } Table;
 
+Table* new_table() {
+  Table* table = (Table*)malloc(sizeof(Table));
+  table->num_rows = 0;
+  for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+    table->pages[i] = NULL;
+  }
+  return table;
+}
+
+void free_table(Table* table) {
+  for (int i = 0; table->pages[i]; i++) {
+    free(table->pages[i]);
+  }
+  free(table);
+}
+
 void* row_slot(Table* table, uint32_t row_num) {
   uint32_t page_num = row_num / ROWS_PER_PAGE;
   void* page = table->pages[page_num];
