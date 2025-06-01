d86e96a3551062835f6471ec97fc5930b542a80e
BeLeap <beleap@beleap.dev>
2025-06-01 17:42:02 +0900

    update cursor

--- db.c
+++ db.c
@@ -296,79 +296,10 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
 
 typedef struct {
   Table* table;
-  uint32_t row_num;
+  uint32_t page_num;
+  uint32_t cell_num;
   bool end_of_table;
 } Cursor;
-
-Cursor* table_start(Table* table) {
-  Cursor* cursor = malloc(sizeof(Cursor));
-  cursor->table = table;
-  cursor->row_num = 0;
-  cursor->end_of_table = (table->num_rows == 0);
-
-  return cursor;
-}
-Cursor* table_end(Table* table) {
-  Cursor* cursor = malloc(sizeof(Cursor));
-  cursor->table = table;
-  cursor->row_num = table->num_rows;
-  cursor->end_of_table = true;
-
-  return cursor;
-}
-void cursor_advance(Cursor* cursor) {
-  cursor->row_num += 1;
-  if (cursor->row_num >= cursor->table->num_rows) {
-    cursor->end_of_table = true;
-  }
-}
-
-void* cursor_value(Cursor* cursor) {
-  uint32_t row_num = cursor->row_num;
-  uint32_t page_num = row_num / ROWS_PER_PAGE;
-  void* page = get_page(cursor->table->pager, page_num);
-  uint32_t row_offset = row_num % ROWS_PER_PAGE;
-  uint32_t byte_offset = row_offset * ROW_SIZE;
-  return page + byte_offset;
-}
-
-ExecuteResult execute_insert(Statement* statement, Table* table) {
-  if (table->num_rows >= TABLE_MAX_ROWS) {
-    return EXECUTE_TABLE_FULL;
-  }
-
-  Row* row_to_insert = &(statement->row_to_insert);
-  Cursor* cursor = table_end(table);
-
-  serialize_row(row_to_insert, cursor_value(cursor));
-  table->num_rows+=1;
-
-  free(cursor);
-
-  return EXECUTE_SUCCESS;
-}
-ExecuteResult execute_select(Statement* statement, Table* table) {
-  Cursor* cursor = table_start(table);
-
-  Row row;
-  while(!(cursor->end_of_table)) {
-    deserialize_row(cursor_value(cursor), &row);
-    print_row(&row);
-    cursor_advance(cursor);
-  }
-
-  free(cursor);
-
-  return EXECUTE_SUCCESS;
-}
-ExecuteResult execute_statement(Statement* statement, Table* table) {
-  switch (statement->type) {
-    case STATEMENT_INSERT:
-      return execute_insert(statement, table);
-    case STATEMENT_SELECT:
-      return execute_select(statement, table);
-  }
-}
 
 typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
 
@@ -416,6 +347,85 @@ void* leaf_node_value(void* node, uint32_t cell_num) {
 void initialize_leaf_node(void* node) {
   *leaf_node_num_cells(node) = 0;
 }
+
+Cursor* table_start(Table* table) {
+  Cursor* cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->page_num = table->root_page_num;
+  cursor->cell_num = 0;
+
+  void* root_node = get_page(table->pager, table->root_page_num);
+  uint32_t num_cells = *leaf_node_num_cells(root_node);
+  cursor->end_of_table = (num_cells == 0);
+
+  return cursor;
+}
+Cursor* table_end(Table* table) {
+  Cursor* cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->page_num = table->root_page_num;
+
+  void* root_node = get_page(table->pager, table->root_page_num);
+  uint32_t num_cells = *leaf_node_num_cells(root_node);
+  cursor->cell_num = num_cells;
+  cursor->end_of_table = true;
+
+  return cursor;
+}
+
+void* cursor_value(Cursor* cursor) {
+  uint32_t page_num = cursor->page_num;
+  void* page = get_page(cursor->table->pager, page_num);
+  return leaf_node_value(page, cursor->cell_num);
+}
+void cursor_advance(Cursor* cursor) {
+  uint32_t page_num = cursor->page_num;
+  void* node = get_page(cursor->table->pager, page_num);
+
+  cursor->cell_num += 1;
+  if (cursor->cell_num >= (*leaf_node_num_cells(node))) {
+    cursor->end_of_table = true;
+  }
+}
+
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+  if (table->num_rows >= TABLE_MAX_ROWS) {
+    return EXECUTE_TABLE_FULL;
+  }
+
+  Row* row_to_insert = &(statement->row_to_insert);
+  Cursor* cursor = table_end(table);
+
+  serialize_row(row_to_insert, cursor_value(cursor));
+  table->num_rows+=1;
+
+  free(cursor);
+
+  return EXECUTE_SUCCESS;
+}
+ExecuteResult execute_select(Statement* statement, Table* table) {
+  Cursor* cursor = table_start(table);
+
+  Row row;
+  while(!(cursor->end_of_table)) {
+    deserialize_row(cursor_value(cursor), &row);
+    print_row(&row);
+    cursor_advance(cursor);
+  }
+
+  free(cursor);
+
+  return EXECUTE_SUCCESS;
+}
+ExecuteResult execute_statement(Statement* statement, Table* table) {
+  switch (statement->type) {
+    case STATEMENT_INSERT:
+      return execute_insert(statement, table);
+    case STATEMENT_SELECT:
+      return execute_select(statement, table);
+  }
+}
+
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
