7e208f89655937e0ab4bccb0f7932ade5fdbb169
BeLeap <beleap@beleap.dev>
2025-06-01 14:34:02 +0900

    add new_input_buffer

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/types.h>
+
+typedef struct {
+  char* buffer;
+  size_t buffer_length;
+  ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+  InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+
+  return input_buffer;
+}
 
 int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
