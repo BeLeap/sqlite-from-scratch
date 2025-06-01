ec0e772f8f67cd8f9bf98bbfdc0a8db7bf699675
BeLeap <beleap@beleap.dev>
2025-06-01 18:24:42 +0900

    set nodetype on init

--- db.c
+++ db.c
@@ -286,6 +286,16 @@ const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
 const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
 const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
+NodeType get_node_type(void* node) {
+  uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+  return (NodeType)value;
+}
+
+void set_node_type(void* node, NodeType type) {
+  uint8_t value = type;
+  *((uint8_t*)node + NODE_TYPE_OFFSET) = value;
+}
+
 uint32_t* leaf_node_num_cells(void* node) {
   return node + LEAF_NODE_NUM_CELLS_OFFSET;
 }
@@ -299,6 +309,7 @@ void* leaf_node_value(void* node, uint32_t cell_num) {
   return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 void initialize_leaf_node(void* node) {
+  set_node_type(node, NODE_LEAF);
   *leaf_node_num_cells(node) = 0;
 }
 
@@ -364,16 +375,6 @@ Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
 
   cursor->cell_num = min_index;
   return cursor;
-}
-
-NodeType get_node_type(void* node) {
-  uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
-  return (NodeType)value;
-}
-
-void set_node_type(void* node, NodeType type) {
-  uint8_t value = type;
-  *((uint8_t*)node + NODE_TYPE_OFFSET) = value;
 }
 
 /*
