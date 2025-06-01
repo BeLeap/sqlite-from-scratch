08ad315ccd2fc85b3aacc71387377fa029a61639
BeLeap <beleap@beleap.dev>
2025-06-01 17:45:01 +0900

    update db_open

--- db.c
+++ db.c
@@ -241,58 +241,10 @@ typedef struct {
   uint32_t root_page_num;
 } Table;
 
-Table* db_open(const char* filename) {
-  Pager* pager = pager_open(filename);
-  uint32_t num_rows = pager->file_length / ROW_SIZE;
-
-  Table* table = (Table*)malloc(sizeof(Table));
-  table-> pager = pager;
-  table->num_rows = num_rows;
-
-  return table;
-}
-
-void db_close(Table* table) {
-  Pager* pager = table->pager;
-
-  for (uint32_t i = 0; i < pager->num_pages; i++) {
-    if (pager->pages[i] == NULL) {
-      continue;
-    }
-    pager_flush(pager, i);
-    free(pager->pages[i]);
-    pager->pages[i] = NULL;
-  }
-
-  int result = close(pager->file_descriptor);
-  if (result == -1) {
-    printf("Error closing db file.\n");
-    exit(EXIT_FAILURE);
-  }
-  for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-    void* page = pager->pages[i];
-    if (page) {
-      free(page);
-      pager->pages[i] = NULL;
-    }
-  }
-  free(pager);
-  free(table);
-}
-
 typedef enum {
   EXECUTE_SUCCESS,
   EXECUTE_TABLE_FULL,
 } ExecuteResult;
-
-MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
-  if (strcmp(input_buffer->buffer, ".exit") == 0) {
-    db_close(table);
-    exit(EXIT_SUCCESS);
-  } else {
-    return META_COMMAND_UNRECOGNIZED_COMMAND;
-  }
-}
 
 typedef struct {
   Table* table;
@@ -426,6 +378,58 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
   }
 }
 
+Table* db_open(const char* filename) {
+  Pager* pager = pager_open(filename);
+
+  Table* table = (Table*)malloc(sizeof(Table));
+  table-> pager = pager;
+  table->root_page_num = 0;
+
+  if (pager->num_pages == 0) {
+    // New database file. Initialize page 0 as leaf node.
+    void* root_node = get_page(pager, 0);
+    initialize_leaf_node(root_node);
+  }
+  
+  return table;
+}
+
+void db_close(Table* table) {
+  Pager* pager = table->pager;
+
+  for (uint32_t i = 0; i < pager->num_pages; i++) {
+    if (pager->pages[i] == NULL) {
+      continue;
+    }
+    pager_flush(pager, i);
+    free(pager->pages[i]);
+    pager->pages[i] = NULL;
+  }
+
+  int result = close(pager->file_descriptor);
+  if (result == -1) {
+    printf("Error closing db file.\n");
+    exit(EXIT_FAILURE);
+  }
+  for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+    void* page = pager->pages[i];
+    if (page) {
+      free(page);
+      pager->pages[i] = NULL;
+    }
+  }
+  free(pager);
+  free(table);
+}
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    db_close(table);
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
